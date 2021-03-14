90d64838e04e3b2e093c252f178df175d8a0f99f
Anahita <shakofa.mahmody@gmail.com>
2021-03-14 00:09:37 -0800

    Find, create, update and remove category on category-rout file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,50 @@
 const router = require('express').Router();
+const { in } = require('sequelize/types/lib/operators');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({include: [product],
+  }).then((categories) => res.json(categories))
+    .catch((err) => res.status(500).json(err));
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  category.findOne({
+    where: {id: req.params.id,},
+    include: [product],
+    include: [category]
+  }).then((category) => res.json(category))
+  .catch((err) => res.status(400).json(err));
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  category.create(res.body).then((category) => 
+  res.status(200).json(category))
+  .catch((err) => 
+    res.status(400).json(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  category.update(req.body, {
+    where: { id:req.params.id},
+  }).then ((category) => res.status(200).json(category)).catch((err) => res.status(400).json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  category.destroy({ 
+    where:{ id: req.params.id,
+    },
+  }).then ((category) => res.status(200).json(category)).catch((err) => res.status(400).json(err));
+  
 });
 
 module.exports = router;
