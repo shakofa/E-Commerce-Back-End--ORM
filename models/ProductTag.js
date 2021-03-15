fe2980c7c955963703d149b672c737fbdc68d0c6
Anahita <shakofa.mahmody@gmail.com>
2021-03-15 00:10:32 -0700

    Define product tag column on productTag.js file.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,31 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id',
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+      validate: {
+        isInteger: true,
+      }
+    }
   },
   {
     sequelize,
