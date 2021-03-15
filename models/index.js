15b1812fb1621bc164c9ea93cc2f1ab12aa267b1
Anahita <shakofa.mahmody@gmail.com>
2021-03-15 07:43:57 -0700

    Add association methods on index.js model file to create the relationship between the models.

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+product.belongTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+category.belongToMany(Tag, {
+  through: 'productTag',
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
+tag.belongsToMany(Produt, {
+  through: 'productTag',
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
